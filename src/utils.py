3a9df9f4d3a93fc39176e5492f96f9d8e3d5daea
sudha7773 <sudharani.vejendla@gmail.com>
2024-10-07 17:51:35 +0530

    final commit to git

--- src/utils.py
+++ src/utils.py
@@ -38,7 +38,6 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
@@ -48,6 +47,15 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
